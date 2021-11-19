8f672b5fdbac8cb47c6ca485a27f1c21c901cf4c
Ayodeji <ayodejiayankola@gmail.com>
2021-11-19 15:48:37 +0100

    created the required  parameter for the api

---
+++ YouTubeMobileClone/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  YouTubeMobileClone
+//
+//  Created by Ayodejii Ayankola on 19/11/2021.
+//
+
+import Foundation
+
+struct Constants {
+    static var API_KEY = ""
+    static var PLAYLIST_ID = "PLMRqhzcHGw1aLoz4pM_Mg2TewmJcMg9ua"
+    static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID) &key=\(Constants.API_KEY)"
+}
+
---
+++ YouTubeMobileClone/Video.swift
@@ -0,0 +1,17 @@
+//
+//  Video.swift
+//  YouTubeMobileClone
+//
+//  Created by Ayodejii Ayankola on 19/11/2021.
+//
+
+import Foundation
+
+struct Video {
+    var videoId = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var publishedDate = ""
+    
+}
